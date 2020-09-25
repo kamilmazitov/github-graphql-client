6cddb1695a2d2b54e0dea6b3c9d2a39a4855f888
Kamil Mazitov <mazitov.kamil.00@mail.ru>
2020-09-25 17:21:42 +0300

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./client";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      {/* <ThemeProvider theme={customTheme}> */}
+      {/* <CSSReset /> */}
+      {/* <Router> */}
+      {/* <Layout> */}
+      {/* <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch> */}
+      {/* </Layout> */}
+      {/* </Router> */}
+      {/* </ThemeProvider> */}
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
