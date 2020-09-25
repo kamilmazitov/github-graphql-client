6a5e3a743a0acf3a037efbb39739d13b249a54f0
Kamil Mazitov <mazitov.kamil.00@mail.ru>
2020-09-25 19:22:41 +0300

    basic search

--- src/components/SearchResults/index.tsx
+++ src/components/SearchResults/index.tsx
@@ -4,48 +4,19 @@ import { gql } from "apollo-boost";
 import { StyledSearchResults } from "./styles";
 import UserTile from "../UserTile";
 
-interface ISearchResult {
-  node: {
-    id: string;
-    name: string;
-    avatarUrl: string;
-  };
-}
-
-interface ISearchResponse {
-  search: {
-    edges: ISearchResult[];
-  };
-}
-
-const SEARCH_USER = gql`
-  query userSearch {
-    search(first: 12, type: USER, query: "thomas") {
-      edges {
-        node {
-          ... on User {
-            id
-            name
-            avatarUrl
-          }
-        }
-      }
-    }
-  }
-`;
-
 const SearchResults = () => {
-  const { data, loading, error } = useQuery<ISearchResponse>(SEARCH_USER);
+  // const { data, loading, error } = useQuery<ISearchResponse>(SEARCH_USER);
 
-  if (loading) return <p>Loading..</p>;
-  if (error) return <p>Error...</p>;
+  // if (loading) return <p>Loading..</p>;
+  // if (error) return <p>Error...</p>;
 
   return (
-    <StyledSearchResults>
-      {data!.search.edges.map(item => {
-        return <UserTile key={item.node.id} item={item} />;
-      })}
-    </StyledSearchResults>
+    <p>hello</p>
+    // <StyledSearchResults>
+    //   {data!.search.edges.map(item => {
+    //     return <UserTile key={item.node.id} item={item} />;
+    //   })}
+    // </StyledSearchResults>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchForm from "../../components/SearchForm";
 import Container from "../../components/Container";
 import SearchResults from "../../components/SearchResults";
+import { useLazyQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import Input from "../../components/Input";
+import UserTile from "../../components/UserTile";
+
+interface ISearchResult {
+  node: {
+    id: string;
+    name: string;
+    avatarUrl: string;
+  };
+}
+
+interface ISearchResponse {
+  search: {
+    edges: ISearchResult[];
+  };
+}
+
+const SEARCH_USER = gql`
+  query userSearch($queryString: String!) {
+    search(query: $queryString, type: USER, first: 15) {
+      edges {
+        node {
+          ... on User {
+            id
+            name
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Home = () => {
-  // const [getDog, { loading, data }] = useLazyQuery(GET_DOG_PHOTO);
+  // const [searchResult, setSearchResult] = useState<any | null>(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [loadSearchResults, { loading, data, error }] = useLazyQuery(
+    SEARCH_USER
+  );
+
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    loadSearchResults({ variables: { queryString: searchTerm } });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <>
       <Container>
         <h1>User search</h1>
-        <SearchForm />
+
+        <form onSubmit={handleSubmit}>
+          <Input value={searchTerm} onChange={e => handleChange(e)} />
+        </form>
       </Container>
       <Container maxWidth={1400}>
-        <SearchResults />
+        {data &&
+          data.search.edges.map((item: any) => {
+            return <UserTile key={item.node.id} item={item} />;
+          })}
       </Container>
     </>
   );
